aa7bc8532a8e75677f4659ba0a6ca287d1baa7f2
RonAdrianAntonio <ronadrian.antonio@gmail.com>
2020-02-29 00:18:56 -0600

    Made run_headpose.sh run with hardlinks

--- Other/run_headpose.sh
+++ Other/run_headpose.sh
@@ -11,9 +11,9 @@ headpose_step1_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/train_step1
 headpose_step2_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/train_step2_headposes.py"
 headpose_step3_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/train_step3_training.py"
 
-headpose_landmark_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/step1_model/prelim_dfdc_split_step1"
-headpose_headpose_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/step2_model/prelim_dfdc_split_step2"
-headpose_final_model_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/final_models/prelim_dfdc_split_model"
+headpose_landmark_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/step1/prelim1"
+headpose_headpose_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/step2/prelim2"
+headpose_final_model_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/final_models/prelim_dfdc_model"
 headpose_run_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/run_test.py"
 headpose_savefile_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/results/prelim_dfdc_split.pickle"
 
@@ -28,11 +28,11 @@ headpose_savefile_loc="/home/leenooks/Documents/Coding/AI_Proj/HeadPose/results/
 #python3 $syms_py_script_loc $dfdc_vid_loc $dfdc_syms_loc
 
 python --version
-optirun python $headpose_step1_loc --real_video_dir=$dfdc_syms_loc/train/REAL --fake_video_dir=$dfdc_syms_loc/train/FAKE --output_landmark_path=$headpose_landmark_loc
+#optirun python $headpose_step1_loc --real_video_dir=$dfdc_syms_loc/train/REAL --fake_video_dir=$dfdc_syms_loc/train/FAKE --output_landmark_path=$headpose_landmark_loc
 
-optirun python $headpose_step2_loc --landmark_info_path=$headpose_landmark_loc --headpose_save_path=$headpose_headpose_loc
+sudo nice -n 5 optirun python $headpose_step2_loc --landmark_info_path=$headpose_landmark_loc --headpose_save_path=$headpose_headpose_loc
 
-optirun python $headpose_step3_loc --headpose_path=$headpose_headpose_loc --model_save_path=$headpose_final_model_loc
+sudo nice -n 5 optirun python $headpose_step3_loc --headpose_path=$headpose_headpose_loc --model_save_path=$headpose_final_model_loc
 
-python $headpose_run_loc --input_dir=$dfdc_syms_loc/test --classifier_path=$headpose_final_model_loc --save_file=$headpose_savefile_loc
+sudo nice -n 5 python $headpose_run_loc --input_dir=$dfdc_syms_loc/test --classifier_path=$headpose_final_model_loc --save_file=$headpose_savefile_loc
 
